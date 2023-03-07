169dbbeedf3ab6d4f1d064a3f1d596093c8ad18d
Miriam <miriameke5@gmail.com>
2023-03-07 14:00:01 -0800

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- *insert_node - Inserts a number into a sorted singly-linked list.
- *@head: A pointer the head of the linked list.
- *@number: The number to insert.
- *Author - Tolulope Fakunle
- *Return: If the function fails - NULL.
- *	Otherwise - a pointer to the new node.
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ * Author - Tolulope Fakunle
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *head, *new;
 
 	new = malloc(sizeof(listint_t));
-
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
@@ -30,5 +29,6 @@ listint_t *insert_node(listint_t **head, int number)
 
 	new->next = node->next;
 	node->next = new;
+
 	return (new);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,14 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  */
 typedef struct listint_s
@@ -18,8 +15,12 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-listit_t *insert_node(listint_t **head, int number);
 
-#endif
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
+
+
+#endif /* LISTS_H */
